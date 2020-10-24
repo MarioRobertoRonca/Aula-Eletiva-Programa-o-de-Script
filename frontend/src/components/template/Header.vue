e6df4cbcc5959872c33ab5592d68c15115fc0f73
Mário Roberto <50268227+MarioRobertoRonca@users.noreply.github.com>
2020-10-23 22:46:22 -0300

    Home page Header, Menu, Content e  Footer

---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+      <a  class="toogle">
+          <i class="fa fa-lg"></i>
+      </a>
+      <h1 class="title">
+          {{ title }}
+      </h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    }
+}
+</script>
+
+<style>
+    .header{
+        grid-area: header;
+        background: linear-gradient(to right, #1254C4 ,#01FFEC);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
